e4321d63cdde6501f0b8f486c6a13d63965f882c
ImogenWren <imogen.wren91@gmail.com>
2023-11-29 13:54:02 +0000

    Update acWebsocketClient.py

--- acWebsocketClient.py
+++ acWebsocketClient.py
@@ -11,13 +11,16 @@ acWebsocketClient
 
 #https://realpython.com/python-sockets/
 
-
+import sys
 import socket
 import time
+import selectors
+import types
 import acGlobals as glbs
 
 parse = glbs.jsonParse
 pack = glbs.jsonPack
+
 
 
 #HOST = "127.0.0.1"  # The server's hostname or IP address
@@ -30,12 +33,79 @@ json_delay = 1   ## time between json messages to server
 connection_error = 0
 
 
+sel = selectors.DefaultSelector()
+messages = [b"Message 1 Client 1", b"message 2 client 1"]
+
 class acWebsocketClient:
     def __init__(self):
         print("Starting AC Unit Refrigeration Rig - Websocket Client")
         self.connection_error = 0
         self.json_delay = 1
-        
+        self.host = HOST
+        self.port = PORT
+
+
+    def start_connections(self, num_conns = 2):
+        server_addr = (self.host, self.port)
+        for i in range(0, num_conns):
+            connid = i + 1
+            print(f"Starting connection {connid} to {server_addr}")
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.setblocking(False)
+            sock.connect_ex(server_addr)
+            events = selectors.EVENT_READ | selectors.EVENT_WRITE
+            data = types.SimpleNamespace(
+                connid=connid,
+                msg_total=sum(len(m) for m in messages),
+                recv_total=0,
+                messages=messages.copy(),
+                outb=b"",
+            )
+            sel.register(sock, events, data=data)
+
+    def service_connection(self, key, mask):
+        sock = key.fileobj
+        data = key.data
+        if mask & selectors.EVENT_READ:
+            recv_data = sock.recv(1024)  # Should be ready to read
+            if recv_data:
+                print(f"Received {recv_data!r} from connection {data.connid}")
+                data.recv_total += len(recv_data)
+            if not recv_data or data.recv_total == data.msg_total:
+                print(f"Closing connection {data.connid}")
+                sel.unregister(sock)
+                sock.close()
+        if mask & selectors.EVENT_WRITE:
+            if not data.outb and data.messages:
+                data.outb = data.messages.pop(0)
+            if data.outb:
+                print(f"Sending {data.outb!r} to connection {data.connid}")
+                sent = sock.send(data.outb)  # Should be ready to write
+                data.outb = data.outb[sent:]
+
+
+wsc = acWebsocketClient()
+
+if len(sys.argv) != 4:
+    print(f"Usage: {sys.argv[0]} <host> <port> <num_connections>")
+    sys.exit(1)
+
+host, port, num_conns = sys.argv[1:4]
+wsc.start_connections(host, int(port), int(num_conns))
+
+try:
+    while True:
+        events = sel.select(timeout=1)
+        if events:
+            for key, mask in events:
+                wsc.service_connection(key, mask)
+        # Check for a socket being monitored to continue.
+        if not sel.get_map():
+            break
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
 
 
 
