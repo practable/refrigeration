d79f5c46e616067c34f487dd4740365a9ee12bd0
ImogenWren <imogen.wren91@gmail.com>
2024-03-28 11:29:52 +0000

    finished V1.3.1 & tested with hardware
    Version 1.3.1: 28/04/2024
    - Removed additional commas from JSON message - BUG FIX
    - Changed double to single quotes to wrap JSON
    - rebooting error detected - stackoverflow?
    - Reduced size of JSON buffer from 720 to 620
    - Fixed rebooting error but check for problem reappearing
    - #TODO: If rebooting issue or stackoverflow reappears devolve pretty print JSON into own function & only load into memory if option is selected
    ```
    Sketch uses 50780 bytes (19%) of program storage space. Maximum is 253952 bytes.
    Global variables use 6921 bytes (84%) of dynamic memory, leaving 1271 bytes for local variables. Maximum is 8192 bytes.
    ```

--- fw/refrigerationFirmware_V1_3_1/jsonFunctions.h
+++ fw/refrigerationFirmware_V1_3_1/jsonFunctions.h
@@ -18,10 +18,10 @@ char newline[3] = { " " };
 
 
 void build_json() {
-  char json_header[] = "\"{";
+  char json_header[] = "\'{";
   char json_buffer[JSON_BUFFER_SIZE] = { "" };  // this needs to be sized correctly 512 is too small 600 seems to work for now
-  char float_buffer[16];
-  char json_footer[] = "}\"";
+  char float_buffer[8];
+  char json_footer[] = "}\'";
   char valves[] = "valves";
   char relays[] = "relays";
   char sensors[] = "sensors";
@@ -29,12 +29,13 @@ void build_json() {
   char pressure[] = "pressure";
   char misc[] = "misc";
   char s_status[] = "status";
+  char s_timestamp[] = "timestamp";
 
 
   // start json string building
-  sprintf(json_buffer, "%s%s  \"timestamp\" : %lu,", json_header, newline, sampleTimestamp);
+  sprintf(json_buffer, "%s%s  \"%s\" : %lu,", json_header, newline,s_timestamp, sampleTimestamp);
 
-
+ // sprintf(json_buffer, "%s%s  \"%s\" : %u,", json_header, newline, s_timestamp, sampleTimestamp);  //removed the l designation for u
   // load valve data
   sprintf(json_buffer, "%s%s  \"%s\" : {", json_buffer, newline, valves);
   for (int i = 0; i < 7; i++) {
@@ -45,35 +46,38 @@ void build_json() {
 
   //load power relay data
   sprintf(json_buffer, "%s%s  \"%s\" : {", json_buffer, newline, relays);
+  // sprintf(json_buffer, "%s%s  \"relays\" : {", json_buffer, newline);   // trying to save space // didnt work
   for (int i = 0; i < 2; i++) {
     sprintf(json_buffer, "%s%s   \"%s\" : %i,", json_buffer, newline, relay_names[i], bool(adam6052_B.g_coilState & 1 << i));
   }
-  sprintf(json_buffer, "%s%s   \"%s\" : %i", json_buffer, newline, relay_names[3], bool(adam6052_B.g_coilState & 1 << 3));  // Added to remove comma from last line of JSON subsection
+  sprintf(json_buffer, "%s%s   \"%s\" : %i", json_buffer, newline, relay_names[2], bool(adam6052_B.g_coilState & 1 << 2));  // Added to remove comma from last line of JSON subsection
   sprintf(json_buffer, "%s%s  },", json_buffer, newline);
 
   // Load sensor data
   sprintf(json_buffer, "%s%s  \"%s\" : {", json_buffer, newline, sensors);
 
+  //sprintf(json_buffer, "%s%s  \"sensors\" : {", json_buffer, newline);
+
   // load pressure sensors
   sprintf(json_buffer, "%s%s    \"%s\" : {", json_buffer, newline, pressure);
-  for (int i = 0; i < NUM_PRESSURE_SENSORS-1; i++) {
+  for (int i = 0; i < NUM_PRESSURE_SENSORS - 1; i++) {
     dtostrf(ps_vals[i], 6, 2, float_buffer);
     sprintf(json_buffer, "%s%s      \"PS%i\" : %6s,", json_buffer, newline, i + 1, float_buffer);
   }
-  dtostrf(ps_vals[NUM_PRESSURE_SENSORS], 6, 2, float_buffer);
-  sprintf(json_buffer, "%s%s      \"PS%i\" : %6s", json_buffer, newline, NUM_PRESSURE_SENSORS+1, float_buffer);
+  dtostrf(ps_vals[NUM_PRESSURE_SENSORS - 1], 6, 2, float_buffer);
+  sprintf(json_buffer, "%s%s      \"PS%i\" : %6s", json_buffer, newline, NUM_PRESSURE_SENSORS, float_buffer);  // Added to remove comma from last line of JSON subsection
 
   sprintf(json_buffer, "%s%s    },", json_buffer, newline);
 
 
   // load temp sensors
   sprintf(json_buffer, "%s%s    \"%s\" : {", json_buffer, newline, temperature);
-  for (int i = 0; i < NUM_TEMP_SENSORS-1; i++) {
+  for (int i = 0; i < NUM_TEMP_SENSORS - 1; i++) {
     dtostrf(ts_vals[i], 6, 2, float_buffer);
     sprintf(json_buffer, "%s%s      \"TS%i\" : %6s,", json_buffer, newline, i + 1, float_buffer);
   }
-  dtostrf(ts_vals[NUM_TEMP_SENSORS], 6, 2, float_buffer);
-  sprintf(json_buffer, "%s%s      \"TS%i\" : %6s", json_buffer, newline, NUM_TEMP_SENSORS+1, float_buffer);
+  dtostrf(ts_vals[NUM_TEMP_SENSORS - 1], 6, 2, float_buffer);
+  sprintf(json_buffer, "%s%s      \"TS%i\" : %6s", json_buffer, newline, NUM_TEMP_SENSORS, float_buffer);  // Added to remove comma from last line of JSON subsection
 
   sprintf(json_buffer, "%s%s    },", json_buffer, newline);
 
@@ -84,8 +88,8 @@ void build_json() {
     dtostrf(misc_vals[i], 6, 2, float_buffer);
     sprintf(json_buffer, "%s%s      \"%s\" : %6s,", json_buffer, newline, misc_names[i], float_buffer);
   }
-dtostrf(misc_vals[5], 6, 2, float_buffer);
-    sprintf(json_buffer, "%s%s      \"%s\" : %6s", json_buffer, newline, misc_names[5], float_buffer);
+  dtostrf(misc_vals[4], 6, 2, float_buffer);
+  sprintf(json_buffer, "%s%s      \"%s\" : %6s", json_buffer, newline, misc_names[4], float_buffer);  // Added to remove comma from last line of JSON subsection
 
   sprintf(json_buffer, "%s%s    }", json_buffer, newline);
 
